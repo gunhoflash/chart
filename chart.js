3167babd158076d738a9fc41cccad8c7c7ad9829
Geonho Kim <gunhoflash@gmail.com>
2019-02-12 14:09:14 +0900

    Test

--- chart.js
+++ chart.js
@@ -1,30 +1,57 @@
+/* colors */
 const color_white2 = '#fafafa';
 const color_gray1 = '#f0f0f0';
 const color_gray2 = '#e0e0e0';
 const color_gray3 = '#cccccc';
 const color_dark1 = '#212121';
 
+/* polynomial functions */
+const linearPolynomial = (x, a, b) => a*x + b;
+const quadraticPolynomial = (x, a, b, c) => a*x*x + b*x + c;
+const cubicPolynomial = (x, a, b, c, d) => a*x*x*x + b*x*x + c*x + d;
+const normalPolynomial = (x, coef) =>
+{
+	let fx = 0, j = coef.length;
+	while (j--)
+		fx += coef[j] * pow(x, coef.length - j - 1);
+	return fx;
+};
+
+/* non-linear functions */
+const pow = (x, n) =>
+{
+	let term = 1;
+	while (n--) term *= x;
+	return term; // return x^n
+};
+
+/* global variables */
 var canvas;
 var context;
 let W;
 let H;
-let xGap = 1;
-let yGap = 1;
-let xRange = [0, 12];
-let yRange = [0, 8];
+let xGap = 100;
+let yGap = 10;
+let xRange = [0, 1000];
+let yRange = [0, 100];
 let points = [
-	[1, 3, '#0072BB', false],
-	[3, 7, '#8FC93A', false],
-	[7, 2, '#E4CC37', false],
-	[9, 4, '#E18335', false]
+	[   1, 45520/48640*100, '#ff0000', false],
+	[  77, 43680/48640*100, '#ff0000', false],
+	[ 690, 37450/48640*100, '#ff0000', false],
+	[ 723, 39310/48640*100, '#ff0000', false],
+	[ 731, 39080/48640*100, '#ff0000', false],
+	[ 758, 28210/48640*100, '#ff0000', false],
+	[ 764, 41260/48640*100, '#ff0000', false]
 ]; // x, y, color, is_hover_now
 
 let equations = [
-	[(x) => linearPolynomial(x, 0.5, 3), '#aa0000'],
+	/*[(x) => linearPolynomial(x, 0.5, 3), '#aa0000'],
 	[(x) => quadraticPolynomial(x, 0.5, -4, 10), '#00aa00'],
-	[(x) => quadraticPolynomial(x, -2, 4, 1), '#0000aa']
+	[(x) => quadraticPolynomial(x, -2, 4, 1), '#0000aa'],
+	[(x) => normalPolynomial(x, [1, -20, 145, -470, 664, -320]), '#00aaaa']*/
+	//[(x) => quadraticPolynomial(x, 0.0141, -0.7480, 10.7215), '#00aa00'],
+	//[(x) => quadraticPolynomial(x, -0.0000080395140, -0.0061041095733, 100.0630714487515), '#00aa00'],
 ];
-
 init = (canvas_obj) =>
 {
 	canvas = canvas_obj;
@@ -72,14 +99,7 @@ drawGrid = (context) =>
 	context.stroke(); // draw lines
 };
 
-drawDots = (context, point_ar) =>
-{
-	point_ar.forEach(point =>
-	{
-		drawDot(context, point[0], point[1], point[2], point[3]);
-	});
-};
-
+drawDots = (context, point_ar) => point_ar.forEach(point => drawDot(context, point[0], point[1], point[2], point[3]));
 drawDot = (context, x, y, color = color_dark1, hover = false) =>
 {
 	let pixels = point2pixel([x, y]);
@@ -91,14 +111,7 @@ drawDot = (context, x, y, color = color_dark1, hover = false) =>
 	context.fill(); // draw lines
 };
 
-drawPolynomials = (context, equation_obj, interval) =>
-{
-	equation_obj.forEach(equation =>
-	{
-		drawPolynomial(context, equation[0], interval, equation[1]);
-	});
-};
-
+drawPolynomials = (context, equation_obj, interval) => equation_obj.forEach(equation => drawPolynomial(context, equation[0], interval, equation[1]));
 drawPolynomial = (context, polynomial, interval, color) =>
 {
 	let x = interval[0];
@@ -142,10 +155,7 @@ mouseEvent = (context) =>
 
 //
 
-log = (o) =>
-{
-	console.log(o);
-};
+log = o => console.log(o);
 
 is_near = (point1, point2, v) =>
 {
@@ -158,8 +168,8 @@ is_near = (point1, point2, v) =>
 
 point2pixel = (point) =>
 {
-	const xPixel = 10 + (W - 20) / (xRange[1] - xRange[0]) * point[0];
-	const yPixel = H - 10 - (H - 20) / (yRange[1] - yRange[0]) * point[1];
+	const xPixel = 10 + (W - 20) / (xRange[1] - xRange[0]) * (point[0] - xRange[0]);
+	const yPixel = H - 10 - (H - 20) / (yRange[1] - yRange[0]) * (point[1] - yRange[0]);
 	return [xPixel, yPixel];
 };
 
@@ -169,8 +179,3 @@ pixel2point = (pixel) =>
 	const yPoint = (pixel[1] + 10 - H) * (yRange[1] - yRange[0]) / (H - 20);
 	return [xPoint, yPoint];
 };
-
-//
-
-const linearPolynomial = (x, a, b) => a*x + b;
-const quadraticPolynomial = (x, a, b, c) => a*x*x + b*x + c;
